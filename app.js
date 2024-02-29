8f4557aa94538cd71fe52ca1c30dd085fc06d9c1
Muhammad Hunain <153491211+MuhammadHunainGanatra@users.noreply.github.com>
2024-03-01 01:07:25 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,5 @@
+alert("Error! Please enter a valid password");
+alert("Weolcome to JS Land \n HappyCoding!");
+alert("Weolcome to JS Land");
+alert("HappyCoding! \n ◻Prevent this page from creating additional dialogs");
+alert("Hello...! I can run JS through my web browser's console");
